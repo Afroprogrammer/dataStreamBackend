781c10accbaaccdc6fd30c0c3b6bb699ec2d7e03
Franklin <forsonfranklin@gmail.com>
2022-05-05 17:33:51 +0000

    initial commit

---
+++ src/main/java/com/example/datastreambackend/DataStreamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.datastreambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStreamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStreamBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datastreambackend/DataStreamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datastreambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStreamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
